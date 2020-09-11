9836257ab58b95a2d6d0fe3fccdda3bdc409c8b9
bfeukouo <blaise.feukouo@soprasteria.com>
2020-09-11 16:17:50 +0200

    des

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, APP_BOOTSTRAP_LISTENER } from '@angular/core';
+import {Student} from '../modele/student';
+
+import {StudentService} from '../student.service'
 
 @Component({
   selector: 'app-students',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+  studentsTableHeader=[
+    {linkId:1, name: 'Registration number'},
+    {linkId:2, name: 'Firstname'},
+    {linkId:3, name: 'Lastname'},
+    {linkId:4, name: 'Phone number'},
+  ];
 
-  constructor() { }
+  students:Student[];
+  selectedStudent: Student;
+
+  constructor(private studentService:StudentService) { }
 
   ngOnInit() {
+    this.loadStudents();
+  }
+  
+  onSelectStudent(student: Student): void {
+    this.selectedStudent = student;
+    //this.messageService.add(`StudentService: Selected student id=${student.id}`);
+  }
+
+  //Appel Anynchrone
+  loadStudents(): void {
+    this.studentService.getAsynchroneStudents().subscribe(students=>this.students=students);    
+  }
+
+  //Appel Synchrone
+  getStudents(): void {
+    this.students = this.studentService.getStudents();
   }
 
 }
