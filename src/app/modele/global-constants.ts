bcaae2c52821c9630e6325241ffafcb186ee1646
bfeukouo <blaise.feukouo@soprasteria.com>
2020-09-15 17:48:33 +0200

    Affichage current classroom d'un student_2

--- src/app/modele/global-constants.ts
+++ src/app/modele/global-constants.ts
@@ -1,7 +1,7 @@
 export class GlobalConstants {
 
     public static currentSchoolYear='currentSchoolYear';
-    public static restBaseUrl='http://localhost:8080/rest';
-    //public static restBaseUrl='https://fblaise-sms.herokuapp.com/rest';   
+    //public static restBaseUrl='http://localhost:8080/rest';
+    public static restBaseUrl='https://fblaise-sms.herokuapp.com/rest';   
 
 }
